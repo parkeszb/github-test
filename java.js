64429954dac804f9fa1bad824b6827602f71cdd0
Zach Parkes <50418739+parkeszb@users.noreply.github.com>
2019-05-16 12:41:21 -0400

    Chins = 7

--- java.js
+++ java.js
@@ -1,7 +1,11 @@
 // Testing GitHub
 
-let chin = 5
+let chin = 5;
 
 console.log(`Number of chins = ${chin}`);
 
 // edit --> testing branches
+
+chin = 7;
+
+console.log(`Number of chins = ${chin}`);
